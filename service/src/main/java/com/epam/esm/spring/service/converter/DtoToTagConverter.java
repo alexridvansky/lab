f40d47900722ae40ad5421f79e123729aa0de9c3
alexridvansky <alexridvansky@gmail.com>
2021-10-02 00:53:00 +0300

    DtoToTagConverter - backward converter added

---
+++ service/src/main/java/com/epam/esm/spring/service/converter/DtoToTagConverter.java
@@ -0,0 +1,16 @@
+package com.epam.esm.spring.service.converter;
+
+import com.epam.esm.spring.repository.model.Tag;
+import com.epam.esm.spring.service.dto.TagDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DtoToTagConverter implements Converter<TagDto, Tag> {
+    @Override
+    public Tag convert(TagDto source) {
+        return Tag.builder()
+                .name(source.getName())
+                .build();
+    }
+}
