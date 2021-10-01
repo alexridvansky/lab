7864f03aab1cd8e149e91538c1b1476daf937c9a
alexridvansky <alexridvansky@gmail.com>
2021-10-02 00:55:53 +0300

    DefaultTagService - general implementation updated

--- service/src/main/java/com/epam/esm/spring/service/DefaultTagService.java
+++ service/src/main/java/com/epam/esm/spring/service/DefaultTagService.java
@@ -1,26 +1,29 @@
 package com.epam.esm.spring.service;
 
 import com.epam.esm.spring.repository.jdbc.dao.TagDao;
-import com.epam.esm.spring.repository.model.Tag;
-import com.epam.esm.spring.service.dto.TagDto;
+import com.epam.esm.spring.service.converter.DtoToTagConverter;
 import com.epam.esm.spring.service.converter.TagToDtoConverter;
+import com.epam.esm.spring.service.dto.TagDto;
 import com.epam.esm.spring.service.exception.EntryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class DefaultTagService implements TagService {
     private final TagDao tagDao;
     private final TagToDtoConverter tagToDtoConverter;
+    private final DtoToTagConverter dtoToTagConverter;
 
     @Autowired
-    public DefaultTagService(TagDao tagDao, TagToDtoConverter tagToDtoConverter) {
+    public DefaultTagService(TagDao tagDao,
+                             TagToDtoConverter tagToDtoConverter,
+                             DtoToTagConverter dtoToTagConverter) {
         this.tagDao = tagDao;
         this.tagToDtoConverter = tagToDtoConverter;
+        this.dtoToTagConverter = dtoToTagConverter;
     }
 
     @Override
@@ -31,14 +34,14 @@ public class DefaultTagService implements TagService {
     }
 
     @Override
-    public TagDto findById(long id) {
+    public TagDto findById(long id) throws EntryNotFoundException {
         return tagDao.findById(id)
                 .map(tagToDtoConverter::convert)
                 .orElseThrow(EntryNotFoundException::new);
     }
 
     @Override
-    public Tag insert(Tag tag) {
-        return tagDao.insert(tag);
+    public TagDto insert(TagDto tagDto) {
+        return tagToDtoConverter.convert(tagDao.insert(dtoToTagConverter.convert(tagDto)));
     }
 }
