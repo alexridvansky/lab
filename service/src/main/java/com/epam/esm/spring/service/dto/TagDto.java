a37f36f05a998f5f2076e7091d1fcef6cda62f6a
alexridvansky <alexridvansky@gmail.com>
2021-10-02 04:16:10 +0300

    TagDto - AllArg and NoArg constructors were added

--- service/src/main/java/com/epam/esm/spring/service/dto/TagDto.java
+++ service/src/main/java/com/epam/esm/spring/service/dto/TagDto.java
@@ -1,12 +1,16 @@
 package com.epam.esm.spring.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TagDto implements Serializable {
     private long id;
     private String name;
