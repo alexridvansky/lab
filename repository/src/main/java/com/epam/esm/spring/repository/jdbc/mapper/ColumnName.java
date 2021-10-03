999ce120f9f1812111e105b54935b5cf1d443e75
alexridvansky <alexridvansky@gmail.com>
2021-10-04 00:45:52 +0300

    ColumnName - class added

---
+++ repository/src/main/java/com/epam/esm/spring/repository/jdbc/mapper/ColumnName.java
@@ -0,0 +1,15 @@
+package com.epam.esm.spring.repository.jdbc.mapper;
+
+public class ColumnName {
+    // Certificate table columns' names
+    static final String C_ID = "id";
+    static final String C_NAME = "name";
+    static final String C_DESCRIPTION = "description";
+    static final String C_PRICE = "price";
+    static final String C_DURATION = "duration";
+    static final String C_CREATE_DATE = "create_date";
+    static final String C_LAST_UPDATE_DATE = "last_update_date";
+    static final String C_TAG_NAME = "t_name";
+
+    private ColumnName() {}
+}
