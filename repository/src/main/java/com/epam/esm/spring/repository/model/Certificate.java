794f24141cca7f943a1790b664fde6675d943088
alexridvansky <alexridvansky@gmail.com>
2021-10-03 16:30:42 +0300

    Certificate - primitive types changed to wrappers

--- repository/src/main/java/com/epam/esm/spring/repository/model/Certificate.java
+++ repository/src/main/java/com/epam/esm/spring/repository/model/Certificate.java
@@ -12,11 +12,10 @@ import java.util.List;
 @SuperBuilder
 @Data
 public class Certificate extends AbstractEntity {
-    private long id;
     private String name;
     private String description;
     private BigDecimal price;
-    private int duration;
+    private Integer duration;
     private LocalDateTime createDate;
     private LocalDateTime lastUpdateDate;
     private List<Tag> tags;
