bcc755cd18e04ee4be838218aabdc9b3a5158025
alexridvansky <alexridvansky@gmail.com>
2021-10-09 11:36:50 +0300

    SQL scripts replaced

--- repository/src/main/resources/sql_scripts/certificate_tag_xref.sql
+++ repository/src/main/resources/sql_scripts/certificate_tag_xref.sql
@@ -1,53 +1,12 @@
--- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
---
--- Host: pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com    Database: frf01yk8akk0vk2d
--- ------------------------------------------------------
--- Server version	8.0.23
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
-
---
--- GTID state at the beginning of the backup 
---
-
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
-
---
--- Table structure for table `certificate_tag_xref`
---
-
-DROP TABLE IF EXISTS `certificate_tag_xref`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `certificate_tag_xref` (
-  `certificate_id` bigint NOT NULL,
-  `tag_id` bigint NOT NULL,
-  PRIMARY KEY (`certificate_id`,`tag_id`),
-  KEY `certificate_tag_xref_ibfk_2` (`tag_id`),
-  CONSTRAINT `certificate_tag_xref_ibfk_1` FOREIGN KEY (`certificate_id`) REFERENCES `gift_certificate` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `certificate_tag_xref_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-10-07 10:58:29
+CREATE TABLE certificate_tag_xref
+(
+    certificate_id          BIGINT,
+    tag_id                  BIGINT,
+    PRIMARY KEY (certificate_id, tag_id),
+    FOREIGN KEY (certificate_id)
+        REFERENCES gift_certificate(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (tag_id)
+        REFERENCES tags(id)
+        ON DELETE CASCADE
+)
---
+++ repository/src/main/resources/sql_scripts/tags.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tags
+(
+    id                  BIGINT AUTO_INCREMENT,
+    name                VARCHAR(100),
+    PRIMARY KEY (id)
+)
