3337a70304ce60140edd876727a14b4ad52f42d7
alexridvansky <alexridvansky@gmail.com>
2021-10-11 17:19:54 +0300

    RepositoryConfigDev - "dev" config changed to embedded database.

--- repository/src/main/java/com/epam/esm/spring/repository/config/RepositoryConfigDev.java
+++ repository/src/main/java/com/epam/esm/spring/repository/config/RepositoryConfigDev.java
@@ -1,54 +1,48 @@
 package com.epam.esm.spring.repository.config;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @ComponentScan("com.epam.esm.spring")
-@PropertySource("classpath:jdbc-dev.properties")
 @Profile("dev")
 public class RepositoryConfigDev {
-    @Value("${db.driver}")
-    private String driverClassName;
-    @Value("${db.url}")
-    private String dbUrl;
-    @Value("${user}")
-    private String dbUsername;
-    @Value("${password}")
-    private String dbPassword;
-    @Value("${pool.size}")
-    private int poolSize;
+    private static final String SQL_TABLE_GC = "classpath:test_gift_certificate.sql";
+    private static final String SQL_TABLE_T = "classpath:test_tag.sql";
+    private static final String SQL_TABLE_XREF = "classpath:test_certificate_tag_xref.sql";
+    private static final String SQL_FILL_UP = "classpath:db_fillup.sql";
 
     @Bean
-    public DataSource dataSource() {
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName(driverClassName);
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(dbUsername);
-        basicDataSource.setPassword(dbPassword);
-        basicDataSource.setInitialSize(poolSize);
+    public EmbeddedDatabase embeddedDatabase() {
+        EmbeddedDatabaseBuilder databaseBuilder = new EmbeddedDatabaseBuilder();
 
-        return basicDataSource;
+        return databaseBuilder
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript(SQL_TABLE_T)
+                .addScript(SQL_TABLE_GC)
+                .addScript(SQL_TABLE_XREF)
+                .addScript(SQL_FILL_UP)
+                .build();
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
+    public JdbcTemplate jdbcTemplate(EmbeddedDatabase embeddedDatabase) {
+        return new JdbcTemplate(embeddedDatabase);
     }
 
     @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
-        dataSourceTransactionManager.setDataSource(dataSource);
-        return dataSourceTransactionManager;
+    public PlatformTransactionManager transactionManager(EmbeddedDatabase embeddedDatabase) {
+        return new DataSourceTransactionManager(embeddedDatabase);
     }
 }
