306a74631820a3a0b8ae6a6515b0451655b32720
alexridvansky <alexridvansky@gmail.com>
2021-10-12 22:17:03 +0300

    SearchRequestValidator - updated bcs of renaming dependent method

--- service/src/main/java/com/epam/esm/spring/service/util/SearchRequestValidator.java
+++ service/src/main/java/com/epam/esm/spring/service/util/SearchRequestValidator.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @Component
 public class SearchRequestValidator {
@@ -13,11 +14,11 @@ public class SearchRequestValidator {
         String sort = params.get("sort");
         String order = params.get("order");
 
-        if (!sort.isEmpty() && CertificateFieldType.names().stream().noneMatch(names -> names.equals(sort))) {
+        if (!sort.isEmpty() && CertificateFieldType.getNames().stream().noneMatch(names -> names.equals(sort))) {
             throw new NotValidSearchRequest();
         }
 
-        if (!order.isEmpty() && Arrays.asList("ASC", "DESC").stream().noneMatch(value -> value.equals(order))) {
+        if (!order.isEmpty() && Stream.of("ASC", "DESC").noneMatch(value -> value.equals(order))) {
             throw new NotValidSearchRequest();
         }
 
