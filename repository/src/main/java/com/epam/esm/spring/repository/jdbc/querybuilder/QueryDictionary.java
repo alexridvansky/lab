f4e3f388cc39a9172463651baa862bfde298d070
alexridvansky <alexridvansky@gmail.com>
2021-10-12 16:29:46 +0300

    replaced by Interface

--- repository/src/main/java/com/epam/esm/spring/repository/jdbc/querybuilder/QueryDictionary.java
+++ repository/src/main/java/com/epam/esm/spring/repository/jdbc/querybuilder/QueryDictionary.java
@@ -1,14 +1,17 @@
 package com.epam.esm.spring.repository.jdbc.querybuilder;
 
-public interface QueryDictionary {
-    String UPDATE = "UPDATE gift_certificate SET ";
-    String WHERE = " WHERE ";
-    String COMMA = ", ";
-    String INJECT = "=?";
-    String SPACE = " ";
-    String QUOTE = "'";
-    String ORDER_BY = " ORDER BY ";
-    String ASC = "asc";
-    String SEARCH_QUERY = "WHERE t.name like ('%%' %s '%%') and (gc.name LIKE concat ('%%', '%s', '%%') " +
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class QueryDictionary {
+    public static final String UPDATE = "UPDATE gift_certificate SET ";
+    public static final String WHERE = " WHERE ";
+    public static final String COMMA = ", ";
+    public static final String INJECT = "=?";
+    public static final String SPACE = " ";
+    public static final String QUOTE = "'";
+    public static final String ORDER_BY = " ORDER BY ";
+    public static final String ASC = "asc";
+    public static final String SEARCH_QUERY = "WHERE t.name like ('%%' %s '%%') and (gc.name LIKE concat ('%%', '%s', '%%') " +
             "or gc.description LIKE concat ('%%', '%s', '%%')) ";
 }
