edd177cafc3bc70e1651fad95294a2a0c7279f09
alexridvansky <alexridvansky@gmail.com>
2021-10-18 21:41:16 +0300

    WebConfig - Spring Boot config updated

--- web/src/main/java/com/epam/esm/spring/web/config/WebConfig.java
+++ web/src/main/java/com/epam/esm/spring/web/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.epam.esm.spring.web.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan("com.epam.esm.spring")
 public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ResourceBundleMessageSource getResourceBundleMessageSource() {
