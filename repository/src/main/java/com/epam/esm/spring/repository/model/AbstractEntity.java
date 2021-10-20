b67a0a5cc65a1a32566ba9f9be85d5209bf2d63e
alexridvansky <alexridvansky@gmail.com>
2021-10-20 19:14:00 +0300

    Entities updated

--- repository/src/main/java/com/epam/esm/spring/repository/model/AbstractEntity.java
+++ repository/src/main/java/com/epam/esm/spring/repository/model/AbstractEntity.java
@@ -1,10 +1,20 @@
 package com.epam.esm.spring.repository.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
+@NoArgsConstructor
 @SuperBuilder
 public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 }
--- repository/src/main/java/com/epam/esm/spring/repository/model/Tag.java
+++ repository/src/main/java/com/epam/esm/spring/repository/model/Tag.java
@@ -2,11 +2,18 @@ package com.epam.esm.spring.repository.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@EqualsAndHashCode(callSuper = true)
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @SuperBuilder
+@Table(name = "tag")
 public class Tag extends AbstractEntity {
     private String name;
 }
