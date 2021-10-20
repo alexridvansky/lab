1899f0e2e119ed7dc2c84c1c2946bdf9b2556dfd
alexridvansky <alexridvansky@gmail.com>
2021-10-20 19:13:17 +0300

    CertificateDto - class updated

--- service/src/main/java/com/epam/esm/spring/service/dto/CertificateDto.java
+++ service/src/main/java/com/epam/esm/spring/service/dto/CertificateDto.java
@@ -5,17 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -58,5 +56,6 @@ public class CertificateDto {
 
     private LocalDateTime lastUpdateDate;
 
+    @Valid
     private List<TagDto> tags;
 }
