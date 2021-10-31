82a0d41f0db72b0ca622220ae43826556fa94e98
alexridvansky <alexridvansky@gmail.com>
2021-10-31 23:20:17 +0300

    ErrorMessage - @UtilityClass lombok annotation added

--- service/src/main/java/com/epam/esm/spring/service/exception/ErrorMessage.java
+++ service/src/main/java/com/epam/esm/spring/service/exception/ErrorMessage.java
@@ -1,6 +1,10 @@
 package com.epam.esm.spring.service.exception;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class ErrorMessage {
+
     public static final String ERROR_ORDER_NOT_FOUND = "error.order_not_found";
     public static final String ERROR_CERTIFICATE_NOT_FOUND = "error.cert_not_found";
     public static final String ERROR_USER_NOT_FOUND = "error.user_not_found";
