214ab3c9cd513e0d40bc05103524fc891cccc0e6
alexridvansky <alexridvansky@gmail.com>
2021-10-31 22:05:33 +0300

    ErrorMessage - constant messages added

---
+++ service/src/main/java/com/epam/esm/spring/service/exception/ErrorMessage.java
@@ -0,0 +1,11 @@
+package com.epam.esm.spring.service.exception;
+
+public class ErrorMessage {
+    public static final String ERROR_ORDER_NOT_FOUND = "error.order_not_found";
+    public static final String ERROR_CERTIFICATE_NOT_FOUND = "error.cert_not_found";
+    public static final String ERROR_USER_NOT_FOUND = "error.user_not_found";
+    public static final String ERROR_TAG_NOT_FOUND = "error.tag_not_found";
+
+    public static final String ID = "ID = ";
+    public static final String NAME = "NAME = ";
+}
