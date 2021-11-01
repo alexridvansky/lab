86e9be757b46dc8e5f82808c98c2dda323333a9a
alexridvansky <alexridvansky@gmail.com>
2021-11-02 02:38:20 +0300

    SearchRequestValidator - validator slightly updated

--- service/src/main/java/com/epam/esm/spring/service/util/SearchRequestValidator.java
+++ service/src/main/java/com/epam/esm/spring/service/util/SearchRequestValidator.java
@@ -1,28 +1,41 @@
 package com.epam.esm.spring.service.util;
 
 import com.epam.esm.spring.repository.jdbc.querybuilder.CertificateFieldType;
+import com.epam.esm.spring.repository.model.CertificateParam;
+import com.epam.esm.spring.service.TagService;
+import com.epam.esm.spring.service.dto.CertificateParamDto;
 import com.epam.esm.spring.service.exception.NotValidSearchRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
 import java.util.stream.Stream;
 
 @Component
 public class SearchRequestValidator {
-    public boolean validateRequest(Map<String, String> params) {
-        String sort = params.get("sort").toUpperCase();
-        String order = params.get("order").toUpperCase();
 
-        if (StringUtils.isNotEmpty(sort) && CertificateFieldType.getNames()
-                .stream().noneMatch(names -> names.equals(sort))) {
-            throw new NotValidSearchRequest(sort);
+    private final TagService tagService;
+
+    public SearchRequestValidator(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    public CertificateParam validateRequest(CertificateParamDto paramDto) {
+
+        if (StringUtils.isNotEmpty(paramDto.getSort()) && CertificateFieldType.getNames()
+                .stream().noneMatch(names -> names.equalsIgnoreCase(paramDto.getSort()))) {
+            throw new NotValidSearchRequest(paramDto.getSort());
         }
 
-        if (StringUtils.isNotEmpty(order) && Stream.of("ASC", "DESC").noneMatch(value -> value.equals(order))) {
-            throw new NotValidSearchRequest(order);
+        if (StringUtils.isNotEmpty(paramDto.getOrder()) && Stream.of("ASC", "DESC").noneMatch(value ->
+                value.equals(paramDto.getOrder()))) {
+            throw new NotValidSearchRequest(paramDto.getOrder());
         }
 
-        return true;
+        return CertificateParam.builder()
+                .tags(paramDto.getTags())
+                .search(paramDto.getSearch())
+                .sort(paramDto.getSort())
+                .order(paramDto.getOrder())
+                .build();
     }
 }
