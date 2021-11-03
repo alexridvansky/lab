93e3f48472feda01c32c3bec75237c824a012fb8
alexridvansky <alexridvansky@gmail.com>
2021-11-03 17:23:24 +0300

    SearchRequestValidator - verification by SortingOrderType ENUM implemented

--- service/src/main/java/com/epam/esm/spring/service/util/SearchRequestValidator.java
+++ service/src/main/java/com/epam/esm/spring/service/util/SearchRequestValidator.java
@@ -1,12 +1,11 @@
 package com.epam.esm.spring.service.util;
 
 import com.epam.esm.spring.repository.jdbc.querybuilder.CertificateFieldType;
+import com.epam.esm.spring.repository.jdbc.querybuilder.SortingOrderType;
 import com.epam.esm.spring.service.dto.CertificateParamDto;
 import com.epam.esm.spring.service.exception.NotValidSearchRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-
-import java.util.stream.Stream;
 
 @Component
 public class SearchRequestValidator {
@@ -18,8 +17,8 @@ public class SearchRequestValidator {
             throw new NotValidSearchRequest(paramDto.getSort());
         }
 
-        if (StringUtils.isNotEmpty(paramDto.getOrder()) && Stream.of("ASC", "DESC").noneMatch(value ->
-                value.equals(paramDto.getOrder().toUpperCase()))) {
+        if (StringUtils.isNotEmpty(paramDto.getOrder()) && SortingOrderType.getNames()
+                .stream().noneMatch(value -> value.equals(paramDto.getOrder().toUpperCase()))) {
             throw new NotValidSearchRequest(paramDto.getOrder());
         }
     }
