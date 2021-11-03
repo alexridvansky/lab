ef24466b943b827097d0537d383ffa8ad6a47fdd
alexridvansky <alexridvansky@gmail.com>
2021-11-03 17:46:19 +0300

    QueryDictionary - one more constant added

--- repository/src/main/java/com/epam/esm/spring/repository/jdbc/querybuilder/QueryDictionary.java
+++ repository/src/main/java/com/epam/esm/spring/repository/jdbc/querybuilder/QueryDictionary.java
@@ -17,5 +17,6 @@ public class QueryDictionary {
     public static final String ID = "id";
     public static final String TAGS = "tags";
     public static final String NAME = "name";
+    public static final String USERNAME = "username";
     public static final String DESCRIPTION = "description";
 }
