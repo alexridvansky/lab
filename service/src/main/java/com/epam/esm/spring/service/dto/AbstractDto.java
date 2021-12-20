2514489ff9bf3fa1f404afa5bb5bf98b3b4d30b1
alexridvansky <alexridvansky@gmail.com>
2021-12-20 18:05:06 +0300

    bug fix test

--- service/src/main/java/com/epam/esm/spring/service/dto/AbstractDto.java
+++ service/src/main/java/com/epam/esm/spring/service/dto/AbstractDto.java
@@ -1,38 +1,15 @@
 package com.epam.esm.spring.service.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.hateoas.Links;
 import org.springframework.hateoas.RepresentationModel;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public abstract class AbstractDto extends RepresentationModel<AbstractDto> {
     private Long id;
-
-    @JsonProperty("links")
-    @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = EmptyLinksFilter.class)
-    @Override
-    public Links getLinks() {
-        return super.getLinks();
-    }
-
-    static class EmptyLinksFilter{
-
-        @Override
-        public boolean equals(Object obj) {
-
-            if (!(obj instanceof Links)) {
-                return false;
-            }
-            Links links = (Links) obj;
-            return links.isEmpty();
-        }
-    }
 }
