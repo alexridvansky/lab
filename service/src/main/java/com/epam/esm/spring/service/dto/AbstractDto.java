36d1e1d13a2881d5e0331a74e88b5292f5428430
alexridvansky <alexridvansky@gmail.com>
2021-12-20 18:55:01 +0300

    bug fix test

--- service/src/main/java/com/epam/esm/spring/service/dto/AbstractDto.java
+++ service/src/main/java/com/epam/esm/spring/service/dto/AbstractDto.java
@@ -1,12 +1,14 @@
 package com.epam.esm.spring.service.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
